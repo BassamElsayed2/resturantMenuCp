3c9f5d5363c6c1e79e3de50dc4bb8611fd4340e2
Bassam-Elsaied <bassame993@gmail.com>
2025-08-05 19:50:49 +0300

    remove get news

--- src/components/news/categories/useCategories.ts
+++ src/components/news/categories/useCategories.ts
@@ -1,9 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCategories } from "../../../../services/apiCategories";
+// import { useQuery } from "@tanstack/react-query";
+// import { getCategories } from "../../../../services/apiCategories";
 
-export function useCategories() {
-  return useQuery({
-    queryKey: ["categories"],
-    queryFn: getCategories,
-  });
-}
+// export function useCategories() {
+//   return useQuery({
+//     queryKey: ["categories"],
+//     queryFn: getCategories,
+//   });
+// }
